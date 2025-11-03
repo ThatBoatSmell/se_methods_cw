9fd8f27ed9bdebee030c5e3533a23ce44b8c62f6
C Smith <126886671+thatboatsmell@users.noreply.github.com>
2025-11-03 16:20:15 +0000

    turning test SQL queries into test functions

--- se_methods_cw/src/main/java/com/napier/sem/App.java
+++ se_methods_cw/src/main/java/com/napier/sem/App.java
@@ -50,45 +50,12 @@ public class App {
         }
     }
 
-    /* public this is going to be a function that executes a predefined SQL query
-
-                    // This is a basic statement to test connecting to the database. I have no idea why capital is returning a number
-                Statement stmt = a.con.createStatement();
-                String strSelect = "SELECT capital, name "
-                        + "FROM country "
-                        + "LIMIT 10";
-                ResultSet rset = stmt.executeQuery(strSelect);
-
-                while (rset.next()) {
-                    Country country = new Country();
-                    country.capital = rset.getString("capital");
-                    country.name = rset.getString("name");
-                    System.out.println("Name = " + country.name + ", Capital = " + country.capital);
-                }
-
-// The top N populated cities in a region where N is provided by the user.
-//The top N populated cities in a country where N is provided by the user.
-//The top N populated cities in a district where N is provided by the user.
-//All the capital cities in the world organised by largest population to smallest.
-//All the capital cities in a continent organised by largest population to smallest.
-//All the capital cities in a region organised by largest to smallest.
-//The top N populated capital cities in the world where N is provided by the user.
-//The top N populated capital cities in a continent where N is provided by the user.
-//The top N populated capital cities in a region where N is provided by the user.
-//The population of people, people living in cities, and people not living in cities in each continent.
-//The population of people, people living in cities, and people not living in cities in each region.
-//The population of people, people living in cities, and people not living in cities in each country.
-    */
-    public static void main(String[] args) {
-
-        App a = new App();
-
-        a.connect();
-        if (a.con != null) {
+    public void test() {
+        if (con != null) {
             try {
 
-                // This is a basic statement to test connecting to the database. I have no idea why capital is returning a number
-                Statement stmt = a.con.createStatement();
+                // This is a basic statement to test connecting to the database.
+                Statement stmt = con.createStatement();
                 String strSelect = "SELECT capital, name "
                         + "FROM country "
                         + "LIMIT 10";
@@ -100,10 +67,17 @@ public class App {
                     country.name = rset.getString("name");
                     System.out.println("Name = " + country.name + ", Capital = " + country.capital);
                 }
+            }
+            catch (SQLException e) {
+                System.out.println("SQL error occurred: " + e.getMessage());
+            }
+        }
+    }
 
-                System.out.println("\nTHIS IS THE END OF THE FIRST QUERY\n");
-
-                Statement stmt2 = a.con.createStatement();
+    public void test2() {
+        if (con != null) {
+            try {
+                Statement stmt2 = con.createStatement();
                 String strSelect2 = "SELECT name, countrycode, district, population "
                         + "FROM city "
                         + "LIMIT 10";
@@ -118,11 +92,17 @@ public class App {
                     city.population = rset2.getInt("population");
                     System.out.println("Name = " + city.name + ", District = " + city.district + ", Population = " + city.population);
                 }
+            }
+            catch (SQLException e) {
+                System.out.println("SQL error occurred: " + e.getMessage());
+            }
+        }
+    }
 
-                System.out.println("\nTHIS IS THE END OF THE SECOND QUERY\n");
-                // this is a test to try joining city to country
-
-                Statement stmt3 = a.con.createStatement();
+    public void test3() {
+        if (con != null) {
+            try {
+                Statement stmt3 = con.createStatement();
 
                 String strSelect3 = "Select country.name, city.name AS capital "
                         + "FROM country "
@@ -137,12 +117,44 @@ public class App {
                     country3.name = rset3.getString("name");
                     System.out.println("Name = " + country3.name + ", Capital = " + country3.capital);
                 }
-                System.out.println("\nTHIS IS THE END OF THE THIRD QUERY\n");
-                a.disconnect();
-
-            } catch (SQLException e) {
+            }
+            catch (SQLException e) {
                 System.out.println("SQL error occurred: " + e.getMessage());
             }
+        }
+    }
+
+// The top N populated cities in a region where N is provided by the user.
+//The top N populated cities in a country where N is provided by the user.
+//The top N populated cities in a district where N is provided by the user.
+//All the capital cities in the world organised by largest population to smallest.
+//All the capital cities in a continent organised by largest population to smallest.
+//All the capital cities in a region organised by largest to smallest.
+//The top N populated capital cities in the world where N is provided by the user.
+//The top N populated capital cities in a continent where N is provided by the user.
+//The top N populated capital cities in a region where N is provided by the user.
+//The population of people, people living in cities, and people not living in cities in each continent.
+//The population of people, people living in cities, and people not living in cities in each region.
+//The population of people, people living in cities, and people not living in cities in each country.
+
+    public static void main(String[] args) {
+
+        App a = new App();
+
+        a.connect();
+        if (a.con != null) {
+                a.test();
+
+                System.out.println("\nTHIS IS THE END OF THE FIRST QUERY\n");
+
+                a.test2();
+
+                System.out.println("\nTHIS IS THE END OF THE SECOND QUERY\n");
+
+                a.test3();
+
+                System.out.println("\nTHIS IS THE END OF THE THIRD QUERY\n");
+                a.disconnect();
 
         }
     }
