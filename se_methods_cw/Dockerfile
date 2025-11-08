c13011a2dcea11f829c8bca489310fe90ef4755b
andriikonovaliuk <40781425@live.napier.ac.uk>
2025-11-08 10:15:22 +0000

    Done: /// All the countries in the world organised by largest population to smallest. ///All the countries in a continent organised by largest population to smallest. /// All the countries in a continent organised by largest population to smallest. /// All the countries in a region organised by largest population to smallest. /// The top N populated countries in the world where N is provided by the user. /// The top N populated countries in a continent where N is provided by the user. /// The top N populated countries in a region where N is provided by the user. /// All the cities in the world organised by largest population to smallest. /// All the cities in a continent organised by largest population to smallest. /// All the cities in a region organised by largest population to smallest. /// All the cities in a country organised by largest population to smallest. /// All the cities in a district organised by largest population to smallest. /// The top N populated cities in the world where N is provided by the user. /// The top N populated cities in a continent where N is provided by the user.

--- se_methods_cw/Dockerfile
+++ se_methods_cw/Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:latest
+FROM amazoncorretto:17
 COPY ./target/semApp.jar /tmp
 WORKDIR /tmp
 ENTRYPOINT ["java", "-jar", "semApp.jar"]
--- se_methods_cw/src/main/java/com/napier/sem/App.java
+++ se_methods_cw/src/main/java/com/napier/sem/App.java
@@ -20,11 +20,9 @@ public class App {
         for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
             try {
-                // Wait a bit for db to start
-                Thread.sleep(1000);
                 // Connect to database
                 // this may need to be changed back to db:3306 instead of localhost:33060 !!!
-                con = DriverManager.getConnection("jdbc:mysql://localhost:33060/world?useSSL=false&allowPublicKeyRetrieval=true", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false&allowPublicKeyRetrieval=true", "root", "example");
                 System.out.println("Successfully connected");
                 // Wait a bit
                 Thread.sleep(1000);
@@ -58,63 +56,247 @@ public class App {
         if (a.con != null) {
             try {
 
-                // This is a basic statement to test connecting to the database. I have no idea why capital is returning a number
-                Statement stmt = a.con.createStatement();
-                String strSelect = "SELECT capital, name "
+                // All the countries in the world organised by largest population to smallest.
+                System.out.println("All the countries in the world organised by largest population to smallest.");
+                Statement stmtLtoS = a.con.createStatement();
+                String strSelectLtoS = "SELECT name, population "
                         + "FROM country "
-                        + "LIMIT 10";
-                ResultSet rset = stmt.executeQuery(strSelect);
-
-                while (rset.next()) {
-                    Country country = new Country();
-                    country.capital = rset.getString("capital");
-                    country.name = rset.getString("name");
-                    System.out.println("Name = " + country.name + ", Capital = " + country.capital);
+                        + "ORDER BY population DESC";
+                ResultSet rsetLtoS = stmtLtoS.executeQuery(strSelectLtoS);
+                while (rsetLtoS.next()) {
+                    Country country1 = new Country();
+                    country1.population = rsetLtoS.getInt("population");
+                    country1.name = rsetLtoS.getString("name");
+                    System.out.println("Name = " + country1.name + ", Population = " + country1.population);
                 }
 
-                System.out.println("\nTHIS IS THE END OF THE FIRST QUERY\n");
 
-                Statement stmt2 = a.con.createStatement();
-                String strSelect2 = "SELECT name, countrycode, district, population "
-                        + "FROM city "
-                        + "LIMIT 10";
-                ResultSet rset2 = stmt2.executeQuery(strSelect2);
-
-                while (rset2.next()) {
-                    City city = new City();
-                    city.name = rset2.getString("name");
-                    // city.countrycode = rset2.getInt("countrycode");
-                    // ", Country = " + city.countrycode +
-                    city.district = rset2.getString("district");
-                    city.population = rset2.getInt("population");
-                    System.out.println("Name = " + city.name + ", District = " + city.district + ", Population = " + city.population);
+                System.out.println("\n\n\n\nAll the countries in a continent organised by largest population to smallest.");
+                Statement stmtStoL = a.con.createStatement();
+                String strSelectStoL = "SELECT name, population "
+                        + "FROM country "
+                        + "ORDER BY population ASC";
+                ResultSet rsetStoL = stmtStoL.executeQuery(strSelectStoL);
+                while (rsetStoL.next()) {
+                    Country country1 = new Country();
+                    country1.population = rsetStoL.getInt("population");
+                    country1.name = rsetStoL.getString("name");
+                    System.out.println("Name = " + country1.name + ", Population = " + country1.population);
                 }
 
-                System.out.println("\nTHIS IS THE END OF THE SECOND QUERY\n");
-                // this is a test to try joining city to country
 
-                Statement stmt3 = a.con.createStatement();
-
-                String strSelect3 = "Select country.name, city.name AS capital "
+                // All the countries in a continent organised by largest population to smallest.
+                System.out.println("\n\n\n\nAll the countries in a continent organised by largest population to smallest.");
+                Statement stmtContinent = a.con.createStatement();
+                String strSelectContinent = "SELECT name, population "
                         + "FROM country "
-                        + "INNER JOIN city ON country.capital = city.id "
-                        + "LIMIT 10";
+                        + "WHERE continent = 'Asia' "
+                        + "ORDER BY population DESC";
+                ResultSet rsetContinent = stmtContinent.executeQuery(strSelectContinent);
+                while (rsetContinent.next()) {
+                    Country country1 = new Country();
+                    country1.population = rsetContinent.getInt("population");
+                    country1.name = rsetContinent.getString("name");
+                    System.out.println("Name = " + country1.name + ", Population = " + country1.population);
+                }
 
-                ResultSet rset3 = stmt3.executeQuery(strSelect3);
+// All the countries in a region organised by largest population to smallest.
+                System.out.println("\n\n\n\nAll the countries in a region organised by largest population to smallest.");
+                Statement stmtRegion = a.con.createStatement();
+                String strSelectRegion = "SELECT name, population "
+                        + "FROM country "
+                        + "WHERE region = 'Caribbean' "
+                        + "ORDER BY population DESC";
+                ResultSet rsetRegion = stmtRegion.executeQuery(strSelectRegion);
+                while (rsetRegion.next()) {
+                    Country country2 = new Country();
+                    country2.population = rsetRegion.getInt("population");
+                    country2.name = rsetRegion.getString("name");
+                    System.out.println("Name = " + country2.name + ", Population = " + country2.population);
+                }
 
-                while (rset3.next()) {
+// The top N populated countries in the world where N is provided by the user.
+                System.out.println("\n\n\n\nThe top N populated countries in the world.");
+                int nWorld = 5; // N provided by user
+                Statement stmtTopN = a.con.createStatement();
+                String strSelectTopN = "SELECT name, population "
+                        + "FROM country "
+                        + "ORDER BY population DESC "
+                        + "LIMIT " + nWorld;
+                ResultSet rsetTopN = stmtTopN.executeQuery(strSelectTopN);
+                while (rsetTopN.next()) {
                     Country country3 = new Country();
-                    country3.capital = rset3.getString("capital");
-                    country3.name = rset3.getString("name");
-                    System.out.println("Name = " + country3.name + ", Capital = " + country3.capital);
+                    country3.population = rsetTopN.getInt("population");
+                    country3.name = rsetTopN.getString("name");
+                    System.out.println("Name = " + country3.name + ", Population = " + country3.population);
                 }
-                System.out.println("\nTHIS IS THE END OF THE THIRD QUERY\n");
+
+// The top N populated countries in a continent where N is provided by the user.
+                System.out.println("\n\n\n\nThe top N populated countries in a continent.");
+                int nContinent = 5; // N provided by user
+                Statement stmtTopNContinent = a.con.createStatement();
+                String strSelectTopNContinent = "SELECT name, population "
+                        + "FROM country "
+                        + "WHERE continent = 'Europe' "
+                        + "ORDER BY population DESC "
+                        + "LIMIT " + nContinent;
+                ResultSet rsetTopNContinent = stmtTopNContinent.executeQuery(strSelectTopNContinent);
+                while (rsetTopNContinent.next()) {
+                    Country country4 = new Country();
+                    country4.population = rsetTopNContinent.getInt("population");
+                    country4.name = rsetTopNContinent.getString("name");
+                    System.out.println("Name = " + country4.name + ", Population = " + country4.population);
+                }
+
+// The top N populated countries in a region where N is provided by the user.
+                System.out.println("\n\n\n\nThe top N populated countries in a region.");
+                int nRegion = 5; // N provided by user
+                Statement stmtTopNRegion = a.con.createStatement();
+                String strSelectTopNRegion = "SELECT name, population "
+                        + "FROM country "
+                        + "WHERE region = 'Western Europe' "
+                        + "ORDER BY population DESC "
+                        + "LIMIT " + nRegion;
+                ResultSet rsetTopNRegion = stmtTopNRegion.executeQuery(strSelectTopNRegion);
+                while (rsetTopNRegion.next()) {
+                    Country country5 = new Country();
+                    country5.population = rsetTopNRegion.getInt("population");
+                    country5.name = rsetTopNRegion.getString("name");
+                    System.out.println("Name = " + country5.name + ", Population = " + country5.population);
+                }
+
+// All the cities in the world organised by largest population to smallest.
+                System.out.println("\n\n\n\nAll the cities in the world organised by largest population to smallest.");
+                Statement stmtCities = a.con.createStatement();
+                String strSelectCities = "SELECT name, population "
+                        + "FROM city "
+                        + "ORDER BY population DESC";
+                ResultSet rsetCities = stmtCities.executeQuery(strSelectCities);
+                while (rsetCities.next()) {
+                    String cityName = rsetCities.getString("name");
+                    int cityPop = rsetCities.getInt("population");
+                    System.out.println("Name = " + cityName + ", Population = " + cityPop);
+                }
+
+// All the cities in a continent organised by largest population to smallest.
+                System.out.println("\n\n\n\nAll the cities in a continent organised by largest population to smallest.");
+                Statement stmtCitiesContinent = a.con.createStatement();
+                String strSelectCitiesContinent = "SELECT city.name, city.population "
+                        + "FROM city "
+                        + "JOIN country ON city.countrycode = country.code "
+                        + "WHERE country.continent = 'Asia' "
+                        + "ORDER BY city.population DESC";
+                ResultSet rsetCitiesContinent = stmtCitiesContinent.executeQuery(strSelectCitiesContinent);
+                while (rsetCitiesContinent.next()) {
+                    String cityName = rsetCitiesContinent.getString("name");
+                    int cityPop = rsetCitiesContinent.getInt("population");
+                    System.out.println("Name = " + cityName + ", Population = " + cityPop);
+                }
+
+// All the cities in a region organised by largest population to smallest.
+                System.out.println("\n\n\n\nAll the cities in a region organised by largest population to smallest.");
+                Statement stmtCitiesRegion = a.con.createStatement();
+                String strSelectCitiesRegion = "SELECT city.name, city.population "
+                        + "FROM city "
+                        + "JOIN country ON city.countrycode = country.code "
+                        + "WHERE country.region = 'Caribbean' "
+                        + "ORDER BY city.population DESC";
+                ResultSet rsetCitiesRegion = stmtCitiesRegion.executeQuery(strSelectCitiesRegion);
+                while (rsetCitiesRegion.next()) {
+                    String cityName = rsetCitiesRegion.getString("name");
+                    int cityPop = rsetCitiesRegion.getInt("population");
+                    System.out.println("Name = " + cityName + ", Population = " + cityPop);
+                }
+
+// All the cities in a country organised by largest population to smallest.
+                System.out.println("\n\n\n\nAll the cities in a country organised by largest population to smallest.");
+                Statement stmtCitiesCountry = a.con.createStatement();
+                String strSelectCitiesCountry = "SELECT name, population "
+                        + "FROM city "
+                        + "WHERE countrycode = 'USA' "
+                        + "ORDER BY population DESC";
+                ResultSet rsetCitiesCountry = stmtCitiesCountry.executeQuery(strSelectCitiesCountry);
+                while (rsetCitiesCountry.next()) {
+                    String cityName = rsetCitiesCountry.getString("name");
+                    int cityPop = rsetCitiesCountry.getInt("population");
+                    System.out.println("Name = " + cityName + ", Population = " + cityPop);
+                }
+
+// All the cities in a district organised by largest population to smallest.
+                System.out.println("\n\n\n\nAll the cities in a district organised by largest population to smallest.");
+                Statement stmtCitiesDistrict = a.con.createStatement();
+                String strSelectCitiesDistrict = "SELECT name, population "
+                        + "FROM city "
+                        + "WHERE district = 'California' "
+                        + "ORDER BY population DESC";
+                ResultSet rsetCitiesDistrict = stmtCitiesDistrict.executeQuery(strSelectCitiesDistrict);
+                while (rsetCitiesDistrict.next()) {
+                    String cityName = rsetCitiesDistrict.getString("name");
+                    int cityPop = rsetCitiesDistrict.getInt("population");
+                    System.out.println("Name = " + cityName + ", Population = " + cityPop);
+                }
+
+// The top N populated cities in the world where N is provided by the user.
+                System.out.println("\n\n\n\nThe top N populated cities in the world.");
+                int nCitiesWorld = 10; // N provided by user
+                Statement stmtTopNCities = a.con.createStatement();
+                String strSelectTopNCities = "SELECT name, population "
+                        + "FROM city "
+                        + "ORDER BY population DESC "
+                        + "LIMIT " + nCitiesWorld;
+                ResultSet rsetTopNCities = stmtTopNCities.executeQuery(strSelectTopNCities);
+                while (rsetTopNCities.next()) {
+                    String cityName = rsetTopNCities.getString("name");
+                    int cityPop = rsetTopNCities.getInt("population");
+                    System.out.println("Name = " + cityName + ", Population = " + cityPop);
+                }
+
+// The top N populated cities in a continent where N is provided by the user.
+                System.out.println("\n\n\n\nThe top N populated cities in a continent.");
+                int nCitiesContinent = 10; // N provided by user
+                Statement stmtTopNCitiesContinent = a.con.createStatement();
+                String strSelectTopNCitiesContinent = "SELECT city.name, city.population "
+                        + "FROM city "
+                        + "JOIN country ON city.countrycode = country.code "
+                        + "WHERE country.continent = 'Europe' "
+                        + "ORDER BY city.population DESC "
+                        + "LIMIT " + nCitiesContinent;
+                ResultSet rsetTopNCitiesContinent = stmtTopNCitiesContinent.executeQuery(strSelectTopNCitiesContinent);
+                while (rsetTopNCitiesContinent.next()) {
+                    String cityName = rsetTopNCitiesContinent.getString("name");
+                    int cityPop = rsetTopNCitiesContinent.getInt("population");
+                    System.out.println("Name = " + cityName + ", Population = " + cityPop);
+                }
+
+
+
+
+
+
                 a.disconnect();
+
+
 
             } catch (SQLException e) {
                 System.out.println("SQL error occurred: " + e.getMessage());
             }
-
         }
+
+
+
     }
 }
+
+
+/// All the countries in a continent organised by largest population to smallest.
+/// All the countries in a region organised by largest population to smallest.
+/// The top N populated countries in the world where N is provided by the user.
+/// The top N populated countries in a continent where N is provided by the user.
+/// The top N populated countries in a region where N is provided by the user.
+/// All the cities in the world organised by largest population to smallest.
+/// All the cities in a continent organised by largest population to smallest.
+/// All the cities in a region organised by largest population to smallest.
+/// All the cities in a country organised by largest population to smallest.
+/// All the cities in a district organised by largest population to smallest.
+/// The top N populated cities in the world where N is provided by the user.
+/// The top N populated cities in a continent where N is provided by the user.
