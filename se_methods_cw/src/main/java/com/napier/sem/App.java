4904f7ce1231d80796204ec1e29b82459d0cfd72
andriikonovaliuk <40781425@live.napier.ac.uk>
2025-11-13 20:11:31 +0000

    Additionally, the following information should be accessible to the organisation:
        The population of the world.
        The population of a continent.
        The population of a region.
        The population of a country.
        The population of a district.
        The population of a city.
    
    Finally, the organisation has asked if it is possible to provide the number of people who speak the following the following languages from greatest number to smallest, including the percentage of the world population:
    
        Chinese.
        English.
        Hindi.
        Spanish.
        Arabic.
    
    Country Report
    
    A country report requires the following columns:
    
        Code.
        Name.
        Continent.
        Region.
        Population.
        Capital.
    
    City Report
    
    A city report requires the following columns:
    
        Name.
        Country.
        District.
        Population.
    
    Capital City Report
    
    A capital city report requires the following columns:
    
        Name.
        Country.
        Population.
    
    Population Report
    
    For the population reports, the following information is requested:
    
        The name of the continent/region/country.
        The total population of the continent/region/country.
        The total population of the continent/region/country living in cities (including a %).
        The total population of the continent/region/country not living in cities (including a %).

--- se_methods_cw/src/main/java/com/napier/sem/App.java
+++ se_methods_cw/src/main/java/com/napier/sem/App.java
@@ -270,7 +270,255 @@ public class App {
 
 
 
+// ===============================================
+// POPULATION QUERIES
+// ===============================================
 
+// The population of the world
+                System.out.println("\n\n\n\nThe population of the world.");
+                Statement stmtWorldPop = a.con.createStatement();
+                String strSelectWorldPop = "SELECT SUM(population) AS total_population FROM country";
+                ResultSet rsetWorldPop = stmtWorldPop.executeQuery(strSelectWorldPop);
+                if (rsetWorldPop.next()) {
+                    long worldPop = rsetWorldPop.getLong("total_population");
+                    System.out.println("World Population = " + worldPop);
+                }
+
+// The population of a continent
+                System.out.println("\n\n\n\nThe population of a continent.");
+                Statement stmtContinentPop = a.con.createStatement();
+                String strSelectContinentPop = "SELECT SUM(population) AS total_population "
+                        + "FROM country "
+                        + "WHERE continent = 'Asia'";
+                ResultSet rsetContinentPop = stmtContinentPop.executeQuery(strSelectContinentPop);
+                if (rsetContinentPop.next()) {
+                    long continentPop = rsetContinentPop.getLong("total_population");
+                    System.out.println("Continent Population (Asia) = " + continentPop);
+                }
+
+// The population of a region
+                System.out.println("\n\n\n\nThe population of a region.");
+                Statement stmtRegionPop = a.con.createStatement();
+                String strSelectRegionPop = "SELECT SUM(population) AS total_population "
+                        + "FROM country "
+                        + "WHERE region = 'Caribbean'";
+                ResultSet rsetRegionPop = stmtRegionPop.executeQuery(strSelectRegionPop);
+                if (rsetRegionPop.next()) {
+                    long regionPop = rsetRegionPop.getLong("total_population");
+                    System.out.println("Region Population (Caribbean) = " + regionPop);
+                }
+
+// The population of a country
+                System.out.println("\n\n\n\nThe population of a country.");
+                Statement stmtCountryPop = a.con.createStatement();
+                String strSelectCountryPop = "SELECT population "
+                        + "FROM country "
+                        + "WHERE code = 'USA'";
+                ResultSet rsetCountryPop = stmtCountryPop.executeQuery(strSelectCountryPop);
+                if (rsetCountryPop.next()) {
+                    long countryPop = rsetCountryPop.getLong("population");
+                    System.out.println("Country Population (USA) = " + countryPop);
+                }
+
+// The population of a district
+                System.out.println("\n\n\n\nThe population of a district.");
+                Statement stmtDistrictPop = a.con.createStatement();
+                String strSelectDistrictPop = "SELECT SUM(population) AS total_population "
+                        + "FROM city "
+                        + "WHERE district = 'California'";
+                ResultSet rsetDistrictPop = stmtDistrictPop.executeQuery(strSelectDistrictPop);
+                if (rsetDistrictPop.next()) {
+                    long districtPop = rsetDistrictPop.getLong("total_population");
+                    System.out.println("District Population (California) = " + districtPop);
+                }
+
+// The population of a city
+                System.out.println("\n\n\n\nThe population of a city.");
+                Statement stmtCityPop = a.con.createStatement();
+                String strSelectCityPop = "SELECT population "
+                        + "FROM city "
+                        + "WHERE name = 'London'";
+                ResultSet rsetCityPop = stmtCityPop.executeQuery(strSelectCityPop);
+                if (rsetCityPop.next()) {
+                    long cityPop = rsetCityPop.getLong("population");
+                    System.out.println("City Population (London) = " + cityPop);
+                }
+
+// ===============================================
+// LANGUAGE SPEAKERS REPORT
+// ===============================================
+
+                System.out.println("\n\n\n\nLanguage speakers from greatest to smallest with world population percentage.");
+
+// First get world population for percentage calculation
+                Statement stmtWorldPopForLang = a.con.createStatement();
+                String strWorldPopForLang = "SELECT SUM(population) AS total_population FROM country";
+                ResultSet rsetWorldPopForLang = stmtWorldPopForLang.executeQuery(strWorldPopForLang);
+                long worldPopulation = 0;
+                if (rsetWorldPopForLang.next()) {
+                    worldPopulation = rsetWorldPopForLang.getLong("total_population");
+                }
+
+// Get language speakers
+                Statement stmtLanguages = a.con.createStatement();
+                String strSelectLanguages = "SELECT cl.language, SUM(c.population * cl.percentage / 100) AS speakers "
+                        + "FROM countrylanguage cl "
+                        + "JOIN country c ON cl.countrycode = c.code "
+                        + "WHERE cl.language IN ('Chinese', 'English', 'Hindi', 'Spanish', 'Arabic') "
+                        + "GROUP BY cl.language "
+                        + "ORDER BY speakers DESC";
+                ResultSet rsetLanguages = stmtLanguages.executeQuery(strSelectLanguages);
+                while (rsetLanguages.next()) {
+                    String language = rsetLanguages.getString("language");
+                    long speakers = rsetLanguages.getLong("speakers");
+                    double percentage = (double) speakers / worldPopulation * 100;
+                    System.out.println("Language = " + language + ", Speakers = " + speakers + ", World % = " + String.format("%.2f", percentage) + "%");
+                }
+
+// ===============================================
+// COUNTRY REPORT
+// ===============================================
+
+                System.out.println("\n\n\n\nCountry Report (Code, Name, Continent, Region, Population, Capital).");
+                Statement stmtCountryReport = a.con.createStatement();
+                String strSelectCountryReport = "SELECT c.code, c.name, c.continent, c.region, c.population, city.name AS capital "
+                        + "FROM country c "
+                        + "LEFT JOIN city ON c.capital = city.id "
+                        + "ORDER BY c.population DESC "
+                        + "LIMIT 20";
+                ResultSet rsetCountryReport = stmtCountryReport.executeQuery(strSelectCountryReport);
+                while (rsetCountryReport.next()) {
+                    String code = rsetCountryReport.getString("code");
+                    String name = rsetCountryReport.getString("name");
+                    String continent = rsetCountryReport.getString("continent");
+                    String region = rsetCountryReport.getString("region");
+                    long population = rsetCountryReport.getLong("population");
+                    String capital = rsetCountryReport.getString("capital");
+                    System.out.println("Code = " + code + ", Name = " + name + ", Continent = " + continent +
+                            ", Region = " + region + ", Population = " + population + ", Capital = " + capital);
+                }
+
+// ===============================================
+// CITY REPORT
+// ===============================================
+
+                System.out.println("\n\n\n\nCity Report (Name, Country, District, Population).");
+                Statement stmtCityReport = a.con.createStatement();
+                String strSelectCityReport = "SELECT city.name, country.name AS country, city.district, city.population "
+                        + "FROM city "
+                        + "JOIN country ON city.countrycode = country.code "
+                        + "ORDER BY city.population DESC "
+                        + "LIMIT 20";
+                ResultSet rsetCityReport = stmtCityReport.executeQuery(strSelectCityReport);
+                while (rsetCityReport.next()) {
+                    String cityName = rsetCityReport.getString("name");
+                    String countryName = rsetCityReport.getString("country");
+                    String district = rsetCityReport.getString("district");
+                    long population = rsetCityReport.getLong("population");
+                    System.out.println("City = " + cityName + ", Country = " + countryName +
+                            ", District = " + district + ", Population = " + population);
+                }
+
+// ===============================================
+// CAPITAL CITY REPORT
+// ===============================================
+
+                System.out.println("\n\n\n\nCapital City Report (Name, Country, Population).");
+                Statement stmtCapitalReport = a.con.createStatement();
+                String strSelectCapitalReport = "SELECT city.name, country.name AS country, city.population "
+                        + "FROM city "
+                        + "JOIN country ON city.id = country.capital "
+                        + "ORDER BY city.population DESC "
+                        + "LIMIT 20";
+                ResultSet rsetCapitalReport = stmtCapitalReport.executeQuery(strSelectCapitalReport);
+                while (rsetCapitalReport.next()) {
+                    String capitalName = rsetCapitalReport.getString("name");
+                    String countryName = rsetCapitalReport.getString("country");
+                    long population = rsetCapitalReport.getLong("population");
+                    System.out.println("Capital = " + capitalName + ", Country = " + countryName +
+                            ", Population = " + population);
+                }
+
+// ===============================================
+// POPULATION REPORTS (with city/non-city breakdown)
+// ===============================================
+
+// Population report for a continent
+                System.out.println("\n\n\n\nPopulation Report - Continent.");
+                Statement stmtPopReportContinent = a.con.createStatement();
+                String strPopReportContinent = "SELECT "
+                        + "c.continent AS name, "
+                        + "SUM(c.population) AS total_population, "
+                        + "SUM(city_pop.city_population) AS city_population "
+                        + "FROM country c "
+                        + "LEFT JOIN (SELECT countrycode, SUM(population) AS city_population FROM city GROUP BY countrycode) city_pop "
+                        + "ON c.code = city_pop.countrycode "
+                        + "WHERE c.continent = 'Europe' "
+                        + "GROUP BY c.continent";
+                ResultSet rsetPopReportContinent = stmtPopReportContinent.executeQuery(strPopReportContinent);
+                if (rsetPopReportContinent.next()) {
+                    String name = rsetPopReportContinent.getString("name");
+                    long totalPop = rsetPopReportContinent.getLong("total_population");
+                    long cityPop = rsetPopReportContinent.getLong("city_population");
+                    long nonCityPop = totalPop - cityPop;
+                    double cityPercent = (double) cityPop / totalPop * 100;
+                    double nonCityPercent = (double) nonCityPop / totalPop * 100;
+                    System.out.println("Continent = " + name);
+                    System.out.println("Total Population = " + totalPop);
+                    System.out.println("City Population = " + cityPop + " (" + String.format("%.2f", cityPercent) + "%)");
+                    System.out.println("Non-City Population = " + nonCityPop + " (" + String.format("%.2f", nonCityPercent) + "%)");
+                }
+
+// Population report for a region
+                System.out.println("\n\n\n\nPopulation Report - Region.");
+                Statement stmtPopReportRegion = a.con.createStatement();
+                String strPopReportRegion = "SELECT "
+                        + "c.region AS name, "
+                        + "SUM(c.population) AS total_population, "
+                        + "SUM(city_pop.city_population) AS city_population "
+                        + "FROM country c "
+                        + "LEFT JOIN (SELECT countrycode, SUM(population) AS city_population FROM city GROUP BY countrycode) city_pop "
+                        + "ON c.code = city_pop.countrycode "
+                        + "WHERE c.region = 'Western Europe' "
+                        + "GROUP BY c.region";
+                ResultSet rsetPopReportRegion = stmtPopReportRegion.executeQuery(strPopReportRegion);
+                if (rsetPopReportRegion.next()) {
+                    String name = rsetPopReportRegion.getString("name");
+                    long totalPop = rsetPopReportRegion.getLong("total_population");
+                    long cityPop = rsetPopReportRegion.getLong("city_population");
+                    long nonCityPop = totalPop - cityPop;
+                    double cityPercent = (double) cityPop / totalPop * 100;
+                    double nonCityPercent = (double) nonCityPop / totalPop * 100;
+                    System.out.println("Region = " + name);
+                    System.out.println("Total Population = " + totalPop);
+                    System.out.println("City Population = " + cityPop + " (" + String.format("%.2f", cityPercent) + "%)");
+                    System.out.println("Non-City Population = " + nonCityPop + " (" + String.format("%.2f", nonCityPercent) + "%)");
+                }
+
+// Population report for a country
+                System.out.println("\n\n\n\nPopulation Report - Country.");
+                Statement stmtPopReportCountry = a.con.createStatement();
+                String strPopReportCountry = "SELECT "
+                        + "c.name AS name, "
+                        + "c.population AS total_population, "
+                        + "COALESCE(city_pop.city_population, 0) AS city_population "
+                        + "FROM country c "
+                        + "LEFT JOIN (SELECT countrycode, SUM(population) AS city_population FROM city GROUP BY countrycode) city_pop "
+                        + "ON c.code = city_pop.countrycode "
+                        + "WHERE c.code = 'USA'";
+                ResultSet rsetPopReportCountry = stmtPopReportCountry.executeQuery(strPopReportCountry);
+                if (rsetPopReportCountry.next()) {
+                    String name = rsetPopReportCountry.getString("name");
+                    long totalPop = rsetPopReportCountry.getLong("total_population");
+                    long nonCityPop = rsetPopReportCountry.getLong("city_population");
+                    long CityPop = totalPop - nonCityPop;
+                    double cityPercent = (double) cityPop / totalPop * 100;
+                    double nonCityPercent = (double) nonCityPop / totalPop * 100;
+                    System.out.println("Country = " + name);
+                    System.out.println("Total Population = " + totalPop);
+                    System.out.println("City Population = " + cityPop + " (" + String.format("%.2f", cityPercent) + "%)");
+                    System.out.println("Non-City Population = " + nonCityPop + " (" + String.format("%.2f", nonCityPercent) + "%)");
+                }
 
 
                 a.disconnect();
